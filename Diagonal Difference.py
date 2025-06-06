f63477ead1fc72204636f961c3327f958d9ae0a9
Tushar Shihab <67703360+tshihab07@users.noreply.github.com>
2025-06-06 19:34:11 +0600

    problem solving section

--- Diagonal Difference.py
+++ Diagonal Difference.py
@@ -6,12 +6,20 @@ import random
 import re
 import sys
 
-#
-# Complete the 'diagonalDifference' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts 2D_INTEGER_ARRAY arr as parameter.
-#
+""" Problem Description:
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix arr is shown below:
+
+1 2 3
+4 5 6
+9 8 9
+
+- The left-to-right diagonal = 1 + 5 + 9 = 16.
+- The right-to-left diagonal = 3+ 5 + 6 = 17.
+Their absolute difference is | 15 - 17 | = 2.
+"""
+
 
 def diagonalDifference(arr):
     left_sum = 0
