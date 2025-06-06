ee282d8c86546afbb37e8f77022749d7d8f3e534
Tushar Shihab <67703360+tshihab07@users.noreply.github.com>
2025-06-06 20:22:34 +0600

    problem solving section

--- Staircase.py
+++ Staircase.py
@@ -6,11 +6,11 @@ import random
 import re
 import sys
 
-#
-# Complete the 'staircase' function below.
-#
-# The function accepts INTEGER n as parameter.
-#
+""" Problem Description:
+Its base and height are both equal to n. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size n.
+"""
 
 def staircase(n):
     for i in range(n):
