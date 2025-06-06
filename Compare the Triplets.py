458b0ebe0ba06bc5a6edfc34b7cc9058b530a85d
Tushar Shihab <67703360+tshihab07@users.noreply.github.com>
2025-06-06 19:29:19 +0600

    problem solving section

--- Compare the Triplets.py
+++ Compare the Triplets.py
@@ -6,14 +6,26 @@ import random
 import re
 import sys
 
-#
-# Complete the 'compareTriplets' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts following parameters:
-#  1. INTEGER_ARRAY a
-#  2. INTEGER_ARRAY b
-#
+""" Problem Description: 
+Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+
+The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+
+The task is to calculate their comparison points by comparing each category:
+
+If a[i] > b[i], then Alice is awarded 1 point.
+If a[i] < b[i], then Bob is awarded 1 point.
+If a[i] = b[i], then neither person receives a point.
+Example
+
+a = [1, 2, 3]
+b = [3, 2, 1]
+
+For elements *0*, Bob is awarded a point because a[0] < b[0].
+For the equal elements a[1] and b[1], no points are earned.
+Finally, for element 2, a[2] > b[2] , so Alice receives a point.
+The return array is [1, 1] with Alice's score first and Bob's second.
+"""
 
 def compareTriplets(a, b):
     al_point = bob_point = 0
