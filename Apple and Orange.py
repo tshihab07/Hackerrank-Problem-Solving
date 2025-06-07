d95b7d22e78bc77855d79293c005fc0c53d76a5f
Tushar Shihab <67703360+tshihab07@users.noreply.github.com>
2025-06-07 23:38:25 +0600

    problem solving section

--- Apple and Orange.py
+++ Apple and Orange.py
@@ -6,17 +6,18 @@ import random
 import re
 import sys
 
-#
-# Complete the 'countApplesAndOranges' function below.
-#
-# The function accepts following parameters:
-#  1. INTEGER s
-#  2. INTEGER t
-#  3. INTEGER a
-#  4. INTEGER b
-#  5. INTEGER_ARRAY apples
-#  6. INTEGER_ARRAY oranges
-#
+""" Project Description:
+Sam's house has an apple tree and an orange tree that yield an abundance of fruit.
+Using the information given below, determine the number of apples and oranges that land on Sam's house.
+
+In the diagram below:
+
+The red region denotes the house, where s is the start point, and t is the endpoint. The apple tree is to the left of the house, and the orange tree is to its right.
+Assume the trees are located on a single point, where the apple tree is at point a, and the orange tree is at point b.
+When a fruit falls from its tree, it lands d units of distance from its tree of origin along the x-axis.
+*A negative value of d means the fruit fell d units to the tree's left, and a positive value of d means it falls d units to the tree's right. *
+"""
+
 
 def countApplesAndOranges(s, t, a, b, apples, oranges):
     # Write your code here
