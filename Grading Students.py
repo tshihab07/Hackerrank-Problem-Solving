dff7fd49dffbb5e8b99864217d5984688b3a90a8
Tushar Shihab <67703360+tshihab07@users.noreply.github.com>
2025-06-07 23:33:21 +0600

    problem solving section

--- Grading Students.py
+++ Grading Students.py
@@ -6,12 +6,16 @@ import random
 import re
 import sys
 
-#
-# Complete the 'gradingStudents' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY grades as parameter.
-#
+""" Project Description:
+HackerLand University has the following grading policy:
+
+Every student receives a grade in the inclusive range from 0 to 100.
+Any grade less than 40 is a failing grade.
+Sam is a professor at the university and likes to round each student's grade according to these rules:
+
+If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+"""
 
 def gradingStudents(grades):
     final_list = []
