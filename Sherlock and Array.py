5ab2f40bf319c8dbd883c051f7a39ff4dd83eaf8
Tushar Shihab <67703360+tshihab07@users.noreply.github.com>
2025-06-08 23:12:54 +0600

    problem solving section

--- Sherlock and Array.py
+++ Sherlock and Array.py
@@ -6,12 +6,19 @@ import random
 import re
 import sys
 
-#
-# Complete the 'balancedSums' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts INTEGER_ARRAY arr as parameter.
-#
+""" Problem Description:
+Watson gives Sherlock an array of integers.
+His challenge is to find an element of the array such that the sum of all elements to the left is equal to the sum of all elements to the right.
+
+Example -
+arr = [5, 6, 8, 11]
+8 is between two subarrays that sum to 11.
+arr[1]
+the ans is [1] since left and right sum is 0.
+You will be given arrays of integers and must determine whether there is an element that meets the criterion.
+If there is, return YES. Otherwise, return NO.
+
+"""
 
 def balancedSums(arr):
     # Write your code here
