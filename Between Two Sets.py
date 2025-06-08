cd1474cf1076a9b3d141d86908eb32b49315833a
Tushar Shihab <67703360+tshihab07@users.noreply.github.com>
2025-06-08 23:07:54 +0600

    problem solving section

--- Between Two Sets.py
+++ Between Two Sets.py
@@ -6,14 +6,24 @@ import random
 import re
 import sys
 
-#
-# Complete the 'getTotalX' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER_ARRAY a
-#  2. INTEGER_ARRAY b
-#
+""" Problem Description:
+There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
+
+- The elements of the first array are all factors of the integer being considered
+- The integer being considered is a factor of all elements of the second array.
+These numbers are referred to as being between the two arrays. Determine how many such numbers exist.
+
+Example - 
+a = [2, 6]
+b = [24, 36]
+
+There are two numbers between the arrays: 6 and 12.
+6%2 = 0. 6%6 = 0. 24%6 = 0. 36%6 = 0 for the first value.
+12%2 = 0. 12%6 = 0. 24%2 = 0. 36%12 = 0 for the second value.
+
+Return 2.
+
+"""
 
 def getTotalX(a, b):
     # Write your code here
