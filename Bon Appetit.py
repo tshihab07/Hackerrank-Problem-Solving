ed4bc656d0c33092eff0e5e004cbad66397cdd34
Tushar Shihab <67703360+tshihab07@users.noreply.github.com>
2025-06-13 23:41:58 +0600

    problem solving section

--- Bon Appetit.py
+++ Bon Appetit.py
@@ -6,12 +6,15 @@ import random
 import re
 import sys
 
-#
-# Complete the 'dayOfProgrammer' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts INTEGER year as parameter.
-#
+""" Problem Description:
+Two friends Anna and Brian, are deciding how to split the bill at a dinner.
+Each will only pay for the items they consume. Brian gets the check and calculates Anna's portion.
+You must determine if his calculation is correct.
+
+For example, assume the bill has the following prices: bill = [2, 4, 6]. Anna declines to eat item k = bill[2], which costs 6.
+If Brian calculates the bill correctly,  Anna will pay (2 + 4) / 2 = 3. If he includes the cost of bill[2], he will calculate (2 + 4 + 6) / 2 = 6.
+In the second case, he should refund 3 to Anna.
+"""
 
 def dayOfProgrammer(year):
     # Write your code here
