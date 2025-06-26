4bed1be7b647bc849f278b36dcaa0f04fd868ca4
tshihab07 <tushar.shihab13@gmail.com>
2025-06-26 23:59:56 +0600

    Improve Code Complexity

--- Problem Solving Section/Picking Numbers.py
+++ Problem Solving Section/Picking Numbers.py
@@ -1,8 +1,4 @@
-import math
 import os
-import random
-import re
-import sys
 
 """ Problem Description:
 Given an array of integers, find the longest subarray where the absolute difference between any two elements is less than or equal to 1.
