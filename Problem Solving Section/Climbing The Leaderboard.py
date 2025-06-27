20bcc7163581f43081789738e08d1033023b324e
tshihab07 <tushar.shihab13@gmail.com>
2025-06-27 23:48:10 +0600

    Update Documentation

--- Problem Solving Section/Climbing The Leaderboard.py
+++ Problem Solving Section/Climbing The Leaderboard.py
@@ -6,14 +6,20 @@ import random
 import re
 import sys
 
-#
-# Complete the 'climbingLeaderboard' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts following parameters:
-#  1. INTEGER_ARRAY ranked
-#  2. INTEGER_ARRAY player
-#
+""" Problem Description:
+An arcade game player wants to climb to the top of the leaderboard and track their ranking.
+The game uses Dense Ranking, so its leaderboard works like this:
+
+The player with the highest score is ranked number 1 on the leaderboard. 
+Players who have equal scores receive the same ranking number, and the next player(s) receive the immediately following ranking number.
+
+Example:
+ranked = [100, 90, 90, 80]
+player = [70, 80, 105]
+The ranked players will have ranks 1, 2, 2, and 3, respectively.
+If the player's scores are 70,  80 and ,  105, their rankings after each game are 4th, 3rd and 1st. Return [4, 3, 1].
+"""
+
 
 def climbingLeaderboard(ranked, player):
     # Write your code here
