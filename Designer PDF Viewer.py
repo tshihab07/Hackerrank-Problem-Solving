5ab5c4d93c70cf500c552275151a41d454759ff7
tshihab07 <tushar.shihab13@gmail.com>
2025-07-01 23:50:43 +0600

    Optimized Solution

---
+++ Designer PDF Viewer.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    # Write your code here
+    max_height = 0
+    for char in word:
+        index = ord(char) - ord('a')  # Get index in h
+        height = h[index]
+        if height > max_height:
+            max_height = height
+
+    return max_height * len(word)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
