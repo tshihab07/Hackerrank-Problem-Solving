ab37a7d0d3b459dce24a4d9ef8cc52b9cf25f0e2
tshihab07 <tushar.shihab13@gmail.com>
2025-07-02 14:10:38 +0600

    Update Description

--- Problem Solving Section/Angry Professor.py
+++ Problem Solving Section/Angry Professor.py
@@ -6,14 +6,19 @@ import random
 import re
 import sys
 
-#
-# Complete the 'angryProfessor' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts following parameters:
-#  1. INTEGER k
-#  2. INTEGER_ARRAY a
-#
+""" Problem Description:
+Angry Professor: Description
+A Discrete Mathematics professor has a class of students.
+Frustrated with their lack of discipline,
+the professor decides to cancel class if fewer than some number of students are present when class starts.
+Arrival times go from on time (ArrivalTime <= 0) to arrived late (ArrivalTime > 0).
+Given the arrival time of each student and a threshhold number of attendees, determine if the class is cancelled.
+Example
+n = 5
+k = 3
+a = [-2, -1, 0, 1, 2]
+The first 3 students arrived on. The last 2 were late. The threshold is 3 students, so class will go on. Return YES.
+"""
 
 def angryProfessor(k, a):
     # Write your code here
