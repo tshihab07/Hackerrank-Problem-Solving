c54d720d8f99dcc1f51a8e5a6d13c1eefd817a60
tshihab07 <tushar.shihab13@gmail.com>
2025-07-02 14:16:49 +0600

    Update Description

--- Problem Solving Section/Beautiful Days at the Movies.py
+++ Problem Solving Section/Beautiful Days at the Movies.py
@@ -7,7 +7,6 @@ import re
 import sys
 
 """ Problem Description:
-Beautiful Days at the Movies: Description
 Lily likes to play games with integers. She has created a new game where she determines the difference between a number and its reverse.
 For instance, given the number 12,  its reverse is 21. Their difference is 9.
 The number 120 reversed is 21, and their difference is 99.
