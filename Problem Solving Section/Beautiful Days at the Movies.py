4bda0baecafbfad54f852348178d039f5b85c3a5
tshihab07 <tushar.shihab13@gmail.com>
2025-07-02 14:12:54 +0600

    Update Description

--- Problem Solving Section/Beautiful Days at the Movies.py
+++ Problem Solving Section/Beautiful Days at the Movies.py
@@ -6,15 +6,19 @@ import random
 import re
 import sys
 
-#
-# Complete the 'beautifulDays' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER i
-#  2. INTEGER j
-#  3. INTEGER k
-#
+""" Problem Description:
+Beautiful Days at the Movies: Description
+Lily likes to play games with integers. She has created a new game where she determines the difference between a number and its reverse.
+For instance, given the number 12,  its reverse is 21. Their difference is 9.
+The number 120 reversed is 21, and their difference is 99.
+She decides to apply her game to decision making.
+She will look at a numbered range of days and will only go to a movie on a beautiful day.
+
+Given a range of numbered days, [i...j] and a number k, determine the number of days in the range that are beautiful.
+Beautiful numbers are defined as numbers where |i - reversed(i)| is evenly divisible by k.
+If a day's value is a beautiful number, it is a beautiful day.
+Return the number of beautiful days in the range.
+"""
 
 def beautifulDays(i, j, k):
     # Write your code here
