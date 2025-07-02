6f823dbf5e10469aa0a24637bc660b58029422a0
tshihab07 <tushar.shihab13@gmail.com>
2025-07-02 14:08:37 +0600

    Update Description

--- Problem Solving Section/Utopian Tree.py
+++ Problem Solving Section/Utopian Tree.py
@@ -6,12 +6,22 @@ import random
 import re
 import sys
 
-#
-# Complete the 'utopianTree' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER n as parameter.
-#
+""" Problem Description:
+Utopian Tree: Description
+The Utopian Tree goes through 2 cycles of growth every year. Each spring, it doubles in height.
+Each summer, its height increases by 1 meter.
+
+A Utopian Tree sapling with a height of 1 meter is planted at the onset of spring. How tall will the tree be after n growth cycles?
+
+For example, if the number of growth cycles is n = 5, the calculations are as follows:
+Period  Height
+0          1
+1          2
+2          3
+3          6
+4          7
+5          14
+"""
 
 def utopianTree(n):
     # Write your code here
