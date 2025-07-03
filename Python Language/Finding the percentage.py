9cc380b981a8b19dbe73eb93551c566580337933
tshihab07 <tushar.shihab13@gmail.com>
2025-07-03 23:28:05 +0600

    Find Average From Dictionary Values

--- Python Language/Finding the percentage.py
+++ Python Language/Finding the percentage.py
@@ -1,3 +1,16 @@
+""" Problem Description:
+The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students.
+Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+
+Example
+marks key:value pairs are
+'alpha': [20, 30, 40]
+'beta': [30, 50, 70]
+
+query_name = 'beta'
+The query_name is 'beta'. beta's average score is (30 + 50 + 70) / 3 = 50.0.
+"""
+
 if __name__ == '__main__':
     n = int(input())
     student_marks = {}
