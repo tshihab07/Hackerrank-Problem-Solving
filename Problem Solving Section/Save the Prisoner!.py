93661d042e564bc89bc81ca381657c318acad65b
tshihab07 <tushar.shihab13@gmail.com>
2025-07-09 23:48:40 +0600

    Optimal Solution

--- Problem Solving Section/Save the Prisoner!.py
+++ Problem Solving Section/Save the Prisoner!.py
@@ -6,15 +6,16 @@ import random
 import re
 import sys
 
-#
-# Complete the 'saveThePrisoner' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER m
-#  3. INTEGER s
-#
+""" Problem Description:
+A jail has a number of prisoners and a number of treats to pass out to them.
+Their jailer decides the fairest way to divide the treats is to seat the prisoners around a circular table in sequentially numbered chairs.
+A chair number will be drawn from a hat.
+Beginning with the prisoner in that chair, one candy will be handed to each prisoner sequentially around the table until all have been distributed.
+
+The jailer is playing a little joke, though. The last piece of candy looks like all the others, but it tastes awful.
+Determine the chair number occupied by the prisoner who will receive that candy.
+
+"""
 
 def saveThePrisoner(n, m, s):
     # Write your code here
