badc7ab2ec26184a121edee587c5870dfc7a9fc2
tshihab07 <tushar.shihab13@gmail.com>
2025-07-09 23:40:15 +0600

    Optimal Solution

---
+++ Problem Solving Section/Viral Advertising.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    shared = 5
+    total_likes = 0
+
+    for day in range(n):
+        likes = shared // 2
+        total_likes += likes
+        shared = likes * 3  # those who liked share with 3 friends
+
+    return total_likes
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
