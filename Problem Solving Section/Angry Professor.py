97f7cd290855c9d350fa5a7429a22c69ceb22f22
tshihab07 <tushar.shihab13@gmail.com>
2025-07-15 23:36:55 +0600

    solve: angryProfessor function

--- Problem Solving Section/Angry Professor.py
+++ Problem Solving Section/Angry Professor.py
@@ -8,7 +8,7 @@ Frustrated with their lack of discipline,
 the professor decides to cancel class if fewer than some number of students are present when class starts.
 Arrival times go from on time (ArrivalTime <= 0) to arrived late (ArrivalTime > 0).
 Given the arrival time of each student and a threshhold number of attendees, determine if the class is cancelled.
-Example
+Example:
 n = 5
 k = 3
 a = [-2, -1, 0, 1, 2]
