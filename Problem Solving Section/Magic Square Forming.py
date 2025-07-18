2fcd4992b52da69e7aff4c1c1f83648dc8eb92fc
tshihab07 <tushar.shihab13@gmail.com>
2025-07-18 23:54:10 +0600

    solve: formingMagicSquare function

--- Problem Solving Section/Magic Square Forming.py
+++ Problem Solving Section/Magic Square Forming.py
@@ -16,7 +16,7 @@ Note: The resulting magic square must contain distinct integers in the inclusive
 """
 
 def formingMagicSquare(s):
-    # Write your code here
+    # All possible 3x3 magic squares
     magic_squares = [
         [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
         [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
