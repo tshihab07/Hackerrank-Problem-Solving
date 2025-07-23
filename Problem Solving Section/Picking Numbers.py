948fc099fc1708ab25f3e320e3286c97d1ab12c7
tshihab07 <tushar.shihab13@gmail.com>
2025-07-23 23:48:16 +0600

    solve: pickingNumbers function

--- Problem Solving Section/Picking Numbers.py
+++ Problem Solving Section/Picking Numbers.py
@@ -2,6 +2,7 @@ import os
 
 """ Problem Description:
 Given an array of integers, find the longest subarray where the absolute difference between any two elements is less than or equal to 1.
+
 Example:
 a = [1, 1, 2, 2, 4, 4, 5, 5, 5]
 There are two subarrays meeting the criterion: [1, 1, 2, 2] and [4, 4, 5, 5, 5]. The maximum length subarray has 5 elements.
