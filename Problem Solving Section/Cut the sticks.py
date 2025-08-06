70f23b91dea54e3eb8bc1058c2584061cac834e9
tshihab07 <tushar.shihab13@gmail.com>
2025-08-06 23:48:50 +0600

    solve: cutTheSticks function

--- Problem Solving Section/Cut the sticks.py
+++ Problem Solving Section/Cut the sticks.py
@@ -6,12 +6,18 @@ import random
 import re
 import sys
 
-#
-# Complete the 'cutTheSticks' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY arr as parameter.
-#
+""" Problem Description:
+You are given a number of sticks of varying lengths.
+You will iteratively cut the sticks into smaller sticks, discarding the shortest pieces until there are none left.
+At each iteration you will determine the length of the shortest stick remaining,
+cut that length from each of the longer sticks and then discard all the pieces of that shortest length.
+When all the remaining sticks are the same length, they cannot be shortened so discard them.
+
+Given the lengths of n sticks, print the number of sticks that are left before each iteration until there are none left.
+Example:
+arr = [1, 2, 3]
+The shortest stick length is 1, so we cut all sticks by 1.
+"""
 
 def cutTheSticks(arr):
     # Write your code here
