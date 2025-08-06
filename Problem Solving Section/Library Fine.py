4376318f1caa1f285e5e4b64dc6076cfcbba4cf8
tshihab07 <tushar.shihab13@gmail.com>
2025-08-06 23:39:45 +0600

    solve: libraryFine function

--- Problem Solving Section/Library Fine.py
+++ Problem Solving Section/Library Fine.py
@@ -6,18 +6,26 @@ import random
 import re
 import sys
 
-#
-# Complete the 'libraryFine' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER d1
-#  2. INTEGER m1
-#  3. INTEGER y1
-#  4. INTEGER d2
-#  5. INTEGER m2
-#  6. INTEGER y2
-#
+"""Problem Description:
+Here is the extracted text from the image:
+
+---
+
+Your local library needs your help!
+Given the expected and actual return dates for a library book, create a program that calculates the fine (if any).
+The fee structure is as follows:
+
+1. If the book is returned on or before the expected return date, no fine will be charged (i.e.: fine = 0).
+2. If the book is returned after the expected return day but still within the same calendar month and year as the expected return date,
+   fine = 15 Hackos × (the number of days late).
+3. If the book is returned after the expected return month but still within the same calendar year as the expected return date,
+   fine = 500 Hackos × (the number of months late).
+4. If the book is returned after the calendar year in which it was expected, there is a fixed fine of 10000 Hackos.
+
+Charges are based only on the least precise measure of lateness.
+For example, whether a book is due January 1, 2017 or December 31, 2017, if it is returned January 1, 2018, that is a year late and the fine would be 10,000 Hackos.
+
+"""
 
 def libraryFine(d1, m1, y1, d2, m2, y2):
     # Write your code here
