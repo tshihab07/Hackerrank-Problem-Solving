36727667330188f22bc1f5d934da08675af61a19
tshihab07 <tushar.shihab13@gmail.com>
2025-08-08 23:51:09 +0600

    Add regex solution: Matching Whitespace & Non-Whitespace Character

---
+++ Regular Expression/MatchingWhitespace.py
@@ -0,0 +1,10 @@
+""" Title: Matching Whitespace & Non-Whitespace Character 
+Task: You have a test string S. The task is to match the pattern XXxXXxXX
+Here, x denotes whitespace characters and X denotes the non-whitespace characters
+"""
+
+Regex_Pattern = r"\S{2}\s\S{2}\s\S{2}"	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
