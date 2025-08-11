bd9b914efd9d1cedf3d0775015f9d1f1449ffbb4
tshihab07 <tushar.shihab13@gmail.com>
2025-08-11 23:59:07 +0600

    solve: equalizeArray function

--- Problem Solving Section/Equalize the Array.py
+++ Problem Solving Section/Equalize the Array.py
@@ -7,6 +7,15 @@ import re
 import sys
 from collections import Counter
 
+""" Problem Description: Given an array of integers,
+determine the minimum number of elements to delete to leave only elements of equal value.
+Example:
+arr =  [1, 2, 2, 3]
+
+Delete the 2 elements 1 and 3 leaving [2, 2].
+If both twos plus either the 1 or the 3 are deleted, it takes 2 deletions to leave either 1 or 3. The minimum number of deletions is 2.
+"""
+
 
 def equalizeArray(arr):
     # Write your code here
