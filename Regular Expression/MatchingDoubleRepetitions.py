7cae05ffcb52508d8823f3bfa6e056b802fc5ca4
tshihab07 <tushar.shihab13@gmail.com>
2025-08-13 23:56:20 +0600

    Add regex solution: Matching {x, y} Repetitions

--- Regular Expression/MatchingDoubleRepetitions.py
+++ Regular Expression/MatchingDoubleRepetitions.py
@@ -1,3 +1,13 @@
+""" Title: Matching {x, y} Repetitions
+Tasks:
+You have a test string S. 
+Your task is to write a regex that will match  using the following conditions:
+
+S should begin with 1 or 2 digits. 
+After that, S should have 3 or more letters (both lowercase and uppercase). 
+Then S should end with up to 3 . symbol(s). You can end with 0 to 3 . symbol(s), inclusively. 
+"""
+
 Regex_Pattern = r'^\d{1,2}[a-zA-Z]{3,}\.{0,3}$'	# Do not delete 'r'.
 
 import re
