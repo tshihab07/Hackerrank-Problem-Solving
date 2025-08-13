33c3c9011bff270709bd62f4c0f0a8672d94c208
tshihab07 <tushar.shihab13@gmail.com>
2025-08-13 23:43:52 +0600

    solve: taumBday function

--- Problem Solving Section/TaumandBday.py
+++ Problem Solving Section/TaumandBday.py
@@ -6,17 +6,16 @@ import random
 import re
 import sys
 
-#
-# Complete the 'taumBday' function below.
-#
-# The function is expected to return a LONG_INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER b
-#  2. INTEGER w
-#  3. INTEGER bc
-#  4. INTEGER wc
-#  5. INTEGER z
-#
+""" Problem Description:
+Taum is planning to celebrate the birthday of his friend, Diksha.
+There are two types of gifts that Diksha wants from Taum: one is black and the other is white.
+To make her happy, Taum has to buy b black gifts and  white gifts.
+
+The cost of each black gift is bc units.
+The cost of every white gift is wc units.
+The cost to convert a black gift into white gift or vice versa is z units.
+Determine the minimum cost of Diksha's gifts.
+"""
 
 def taumBday(b, w, bc, wc, z):
     # Write your code here
