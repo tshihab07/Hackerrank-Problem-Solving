d4b4b4b86f83e54c2a18278590dd73b3571ee755
tshihab07 <tushar.shihab13@gmail.com>
2025-08-13 23:51:41 +0600

    Add regex solution: Matching {x} Repetitions

--- Regular Expression/MatchingSingleRepetitions.py
+++ Regular Expression/MatchingSingleRepetitions.py
@@ -1,4 +1,12 @@
+""" Title: Matching {x} Repetitions
+Tasks:
+You have a test string S. 
+Your task is to write a regex that will match  using the following conditions:
 
+S must be of length equal to 45.
+The first 40 characters should consist of letters(both lowercase and uppercase), or of even digits. 
+The last 5 characters should consist of odd digits or whitespace characters.
+"""
 
 Regex_Pattern = r'^[a-zA-Z02468]{40}[13579\s]{5}$'	# Do not delete 'r'.
 
