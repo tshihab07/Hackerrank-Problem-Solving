e1ba84f6cba9c6e032e69d197f719654ba6cc758
tshihab07 <tushar.shihab13@gmail.com>
2025-08-13 23:35:44 +0600

    solve: queensAttack function

---
+++ Problem Solving Section/QueensAttack2.py
@@ -0,0 +1,67 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'queensAttack' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER r_q
+#  4. INTEGER c_q
+#  5. 2D_INTEGER_ARRAY obstacles
+#
+
+def queensAttack(n, k, r_q, c_q, obstacles):
+    # Write your code here
+    obstacles = set(map(tuple, obstacles))
+    
+    moves = [
+        (1, 0), (-1, 0),   # up, down
+        (0, 1), (0, -1),   # right, left
+        (1, 1), (1, -1),   # up-right, up-left
+        (-1, 1), (-1, -1)  # down-right, down-left
+    ]
+    
+    count = 0
+    for dr, dc in moves:
+        r, c = r_q + dr, c_q + dc
+        while 1 <= r <= n and 1 <= c <= n and (r, c) not in obstacles:
+            count += 1
+            r += dr
+            c += dc
+    
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    r_q = int(second_multiple_input[0])
+
+    c_q = int(second_multiple_input[1])
+
+    obstacles = []
+
+    for _ in range(k):
+        obstacles.append(list(map(int, input().rstrip().split())))
+
+    result = queensAttack(n, k, r_q, c_q, obstacles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
