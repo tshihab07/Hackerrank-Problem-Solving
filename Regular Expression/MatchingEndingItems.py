7dc6b3654f76023f0ea1f69fd13458f35effb3a5
tshihab07 <tushar.shihab13@gmail.com>
2025-08-14 23:58:54 +0600

    Add regex solution: Matching Ending Items

---
+++ Regular Expression/MatchingEndingItems.py
@@ -0,0 +1,15 @@
+"""Title: Matching Ending Items
+Tasks:
+Write a RegEx to match a test string, S, under the following conditions: 
+
+S should consist of only lowercase and uppercase letters (no numbers or symbols). 
+S should end in s.
+"""
+
+
+# \b indicates the boundary of the pattern
+Regex_Pattern = r'\b[a-zA-Z]*s\b'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
