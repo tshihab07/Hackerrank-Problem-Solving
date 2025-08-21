33ba92e7669288261d10090f7f971dbbd923c7f2
tshihab07 <tushar.shihab13@gmail.com>
2025-08-21 23:49:23 +0600

    Add regex solution: Matching Word & Non-Word Character

--- Regular Expression/MatchingWordNonWord.py
+++ Regular Expression/MatchingWordNonWord.py
@@ -3,7 +3,7 @@ Task: You have a test string S. The task is to match the pattern xxxXxxxxxxxxxxX
 Here, x denotes word characters and X denotes the non-word characters
 """
 
-Regex_Pattern = r"\w{3}\W\w+\W\w{3}"	# Do not delete 'r'.
+Regex_Pattern = r"\w{3}\W\w+\W\w{3}"
 
 import re
 
