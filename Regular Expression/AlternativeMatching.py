2a87f5d85436ecf9eb306cf4d1b1219d303daaa0
tshihab07 <tushar.shihab13@gmail.com>
2025-09-01 23:52:19 +0600

    Add regex solution: Alternative Matching

--- Regular Expression/AlternativeMatching.py
+++ Regular Expression/AlternativeMatching.py
@@ -1,3 +1,12 @@
+""" Title: Alternative Matching
+
+Tasks:
+Given a test string, S, write a RegEx that matches S under the following conditions: 
+
+- S must start with Mr., Mrs., Ms., Dr. or Er.. 
+- The rest of the string must contain only one or more English alphabetic letters (upper and lowercase).
+"""
+
 Regex_Pattern = r'^(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[a-zA-Z]+$'	# Do not delete 'r'.
 
 import re
