a4744517d40fb87a80965918986c5bd1e5905b6d
Tushar Shihab <67703360+tshihab07@users.noreply.github.com>
2025-09-09 23:29:54 +0600

    solve: encryption function

---
+++ Problem Solving Section/Encryption.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def encryption(s):
+    # remove spaces
+    s = s.replace(" ", "")
+    L = len(s)
+    
+    # determine rows and columns
+    rows = math.floor(math.sqrt(L))
+    cols = math.ceil(math.sqrt(L))
+    
+    if rows * cols < L:
+        rows += 1
+    
+    # build the encrypted message
+    encrypted = []
+    for c in range(cols):
+        word = ''.join(s[i] for i in range(c, L, cols))
+        encrypted.append(word)
+    
+    return ' '.join(encrypted)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = encryption(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
