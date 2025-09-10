769da1ee5e589b74916acd0019244f4f0fc76df2
Tushar Shihab <67703360+tshihab07@users.noreply.github.com>
2025-09-10 23:48:09 +0600

    Add regex solution: Backreferences To Failed Groups

--- Regular Expression/Backreferences.py
+++ Regular Expression/Backreferences.py
@@ -7,8 +7,9 @@ Your task is to write a regex which will match S, with the following condition(s
 - S may have "-" separator such that string S gets divided in 4 parts, with each part having exactly two digits. (Eg. 12-34-56-78)
 """
 
-Regex_Pattern = r"^(\d{2})(-?)(\d{2})\2(\d{2})\2(\d{2})$"	# Do not delete 'r'.
+Regex_Pattern = r"^(\d{2})(-?)(\d{2})\2(\d{2})\2(\d{2})$"
 
 import re
+
 
 print(str(bool(re.search(Regex_Pattern, input()))).lower())
