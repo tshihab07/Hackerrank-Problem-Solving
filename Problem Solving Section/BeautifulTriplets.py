ac9fb09f1c46bec2d871a3a89df81848f6e6034f
tshihab07 <tushar.shihab13@gmail.com>
2025-09-22 23:48:13 +0600

    solve: beautifulTriplets function

---
+++ Problem Solving Section/BeautifulTriplets.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def beautifulTriplets(d, arr):
+    # Write your code here
+    s = set(arr)  # for O(1) lookups
+    count = 0
+    for x in arr:
+        if (x + d in s) and (x + 2*d in s):
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = beautifulTriplets(d, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
