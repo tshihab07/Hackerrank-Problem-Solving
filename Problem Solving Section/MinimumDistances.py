f37f8ebc75f636a782e1f1624556cc7eaa1bee80
tshihab07 <tushar.shihab13@gmail.com>
2025-09-22 23:52:00 +0600

    solve: minimumDistances function

--- Problem Solving Section/MinimumDistances.py
+++ Problem Solving Section/MinimumDistances.py
@@ -1,3 +1,7 @@
+""" Problem Description: The distance between two array values is the number of indices between them.
+Given a, find the minimum distance between any pair of equal elements in the array. If no such value exists, return -1.
+"""
+
 #!/bin/python3
 
 import math
