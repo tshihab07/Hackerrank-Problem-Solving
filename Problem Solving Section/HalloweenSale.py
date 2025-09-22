2d24cc97a0e4e5e420f0d9a2538453d993a71ea2
tshihab07 <tushar.shihab13@gmail.com>
2025-09-22 23:56:27 +0600

    solve: howManyGames function

--- Problem Solving Section/HalloweenSale.py
+++ Problem Solving Section/HalloweenSale.py
@@ -1,3 +1,13 @@
+""" Problem Description:
+You wish to buy video games from the famous online video game store Mist.
+
+Usually, all games are sold at the same price, p dollars.
+However, they are planning to have the seasonal Halloween Sale next month in which you can buy games at a cheaper price.
+Specifically, the first game will cost p dollars, and every subsequent game will cost d dollars less than the previous one.
+This continues until the cost becomes less than or equal to  dollars, after which every game will cost m dollars.
+How many games can you buy during the Halloween Sale?
+"""
+
 #!/bin/python3
 
 import math
