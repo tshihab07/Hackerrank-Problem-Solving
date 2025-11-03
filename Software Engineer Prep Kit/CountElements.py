d2aaa777239a4779d1b18a540d3fdf478b48261e
tshihab07 <tushar.shihab13@gmail.com>
2025-11-03 23:44:59 +0600

    solve: countResponseTimeRegressions

---
+++ Software Engineer Prep Kit/CountElements.py
@@ -0,0 +1,38 @@
+""" Problem Description: Given an array of positive integers,
+return the number of elements that are strictly greater than the average of all previous elements.
+Skip the first element."""
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def countResponseTimeRegressions(responseTimes):
+    if not responseTimes:
+        return 0
+    
+    count = 0
+    running_sum = 0
+    
+    for i in range(1, len(responseTimes)):
+        running_sum += responseTimes[i - 1]          # sum of elements before index i
+        previous_avg = running_sum / i      # average of first i elements (indices 0 to i-1)
+        if responseTimes[i] > previous_avg:
+            count += 1
+            
+    return count
+
+if __name__ == '__main__':
+    responseTimes_count = int(input().strip())
+
+    responseTimes = []
+
+    for _ in range(responseTimes_count):
+        responseTimes_item = int(input().strip())
+        responseTimes.append(responseTimes_item)
+
+    result = countResponseTimeRegressions(responseTimes)
+
+    print(result)
