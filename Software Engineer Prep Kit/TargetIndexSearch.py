d112bb40d105270751b70e36f16ff3daf31dd0b8
tshihab07 <tushar.shihab13@gmail.com>
2025-11-06 23:30:30 +0600

    solve: binarySearch function

---
+++ Software Engineer Prep Kit/TargetIndexSearch.py
@@ -0,0 +1,41 @@
+""" Title: Target Index Search
+Given a sorted array of distinct integers and a target value, return the index of the target or -1 if not found. """
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def binarySearch(nums, target):
+    left, right = 0, len(nums) - 1
+    
+    while left <= right:
+        mid = left + (right - left) // 2
+        
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return -1
+
+if __name__ == '__main__':
+    nums_count = int(input().strip())
+
+    nums = []
+
+    for _ in range(nums_count):
+        nums_item = int(input().strip())
+        nums.append(nums_item)
+
+    target = int(input().strip())
+
+    result = binarySearch(nums, target)
+
+    print(result)
