2efefa59d10af094a2d75cb4a13bf32d71a7ece7
tshihab07 <tushar.shihab13@gmail.com>
2025-11-06 23:42:15 +0600

    solve: maximizeNonOverlappingMeetings function

---
+++ Software Engineer Prep Kit/MaximumNumber.py
@@ -0,0 +1,40 @@
+""" Title: Maximum Number of Non-Overlapping Intervals
+Given an array of intervals where each interval has a start and end time, return the maximum number of non-overlapping intervals."""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def maximizeNonOverlappingMeetings(meetings):
+    if not meetings:
+        return 0
+    
+    meetings_sorted = sorted(meetings, key=lambda x: x[1])
+    
+    count = 0
+    last_end = float('-inf')
+    
+    for start, end in meetings_sorted:
+        if start >= last_end:
+            count += 1
+            last_end = end
+    
+    return count
+
+if __name__ == '__main__':
+    meetings_rows = int(input().strip())
+    meetings_columns = int(input().strip())
+
+    meetings = []
+
+    for _ in range(meetings_rows):
+        meetings.append(list(map(int, input().rstrip().split())))
+
+    result = maximizeNonOverlappingMeetings(meetings)
+
+    print(result)
