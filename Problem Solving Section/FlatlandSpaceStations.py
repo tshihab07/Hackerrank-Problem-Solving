16913fce99192b2e94b2be9cb0e016fd4dd663f7
tshihab07 <tushar.shihab13@gmail.com>
2025-12-09 23:43:17 +0600

    solve: flatlandSpaceStations function

--- Problem Solving Section/FlatlandSpaceStations.py
+++ Problem Solving Section/FlatlandSpaceStations.py
@@ -6,6 +6,12 @@ import random
 import re
 import sys
 
+""" Description: Flatland Space Stations" for the description "Flatland is a country with a number of cities,
+some of which have space stations. Cities are numbered consecutively and each has a road of 1km length connecting it to the next city.
+It is not a circular route, so the first city doesn't connect with the last city.
+Determine the maximum distance from any city to its nearest space station."""
+
+
 # Complete the flatlandSpaceStations function below.
 def flatlandSpaceStations(n, c):
     if len(c) == n:
