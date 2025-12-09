5aa88569baefaf949a355a8b3cfaa99cea28bedb
tshihab07 <tushar.shihab13@gmail.com>
2025-12-09 23:59:54 +0600

    solve: cavityMap function

--- Problem Solving Section/CavityMap.py
+++ Problem Solving Section/CavityMap.py
@@ -6,12 +6,12 @@ import random
 import re
 import sys
 
-#
-# Complete the 'cavityMap' function below.
-#
-# The function is expected to return a STRING_ARRAY.
-# The function accepts STRING_ARRAY grid as parameter.
-#
+""" Description: You are given a square map as a matrix of integer strings.
+Each cell of the map has a value denoting its depth.
+We will call a cell of the map a cavity if and only if this cell is not on the border of the map and each cell adjacent to it has strictly smaller depth.
+Two cells are adjacent if they have a common side, or edge.
+Find all the cavities on the map and replace their depths with the uppercase character X
+"""
 
 def cavityMap(grid):
     n = len(grid)
