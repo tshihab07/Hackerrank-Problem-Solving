f0e1d3bf8d31b94d2aa151485d45c2225e5b992a
tshihab07 <tushar.shihab13@gmail.com>
2025-12-10 23:55:33 +0600

    solve: happyLadybugs function

--- Problem Solving Section/Happy Ladybugs.py
+++ Problem Solving Section/Happy Ladybugs.py
@@ -6,12 +6,17 @@ import random
 import re
 import sys
 
-#
-# Complete the 'happyLadybugs' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts STRING b as parameter.
-#
+"""Description: Happy Ladybugs is a board game having the following properties:
+
+- The board is represented by a string, b, of length n. The i-th character of the string, b[i], denotes the i-th cell of the board.
+- If b[i] is an underscore (i.e., _), it means the i-th cell of the board is empty.
+- If b[i] is an uppercase English alphabetic letter (ascii[A-Z]), it means the i-th cell contains a ladybug of color b[i].
+- String b will not contain any other characters.
+- A ladybug is happy only when its left or right adjacent cell (i.e., b[i+/-1) is occupied by another ladybug having the same color.
+- In a single move, you can move a ladybug from its current position to any empty cell.
+Given the values of n and b for g games of Happy Ladybugs, determine if it's possible to make all the ladybugs happy.
+For each game, return YES if all the ladybugs can be made happy through some number of moves. Otherwise, return NO.
+"""
 
 def happyLadybugs(b):
     
