38e72d6c6ec9182d512585f72edd3dc610624aad
tshihab07 <tushar.shihab13@gmail.com>
2025-12-10 23:43:58 +0600

    solve: stones function

--- Problem Solving Section/Manasa and Stones.py
+++ Problem Solving Section/Manasa and Stones.py
@@ -6,16 +6,11 @@ import random
 import re
 import sys
 
-#
-# Complete the 'stones' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER a
-#  3. INTEGER b
-#
-
+""" Description: Manasa is out on a hike with friends. She finds a trail of stones with numbers on them.
+She starts following the trail and notices that any two consecutive stones' numbers differ by one of two values.
+Legend has it that there is a treasure trove at the end of the trail.
+If Manasa can guess the value of the last stone, the treasure will be hers.
+"""
 def stones(n, a, b):
     # Number of steps = n - 1
     steps = n - 1
