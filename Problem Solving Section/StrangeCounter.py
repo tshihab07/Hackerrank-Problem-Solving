9c264616170963e011d5ca129ec495e58dff3045
tshihab07 <tushar.shihab13@gmail.com>
2025-12-11 23:38:38 +0600

    solve: strangeCounter function

---
+++ Problem Solving Section/StrangeCounter.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'strangeCounter' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER t as parameter.
+#
+
+def strangeCounter(t):
+    start_time = 1
+    value = 3
+    while t >= start_time + value:
+        start_time += value
+        value *= 2
+    return value - (t - start_time)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    result = strangeCounter(t)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
