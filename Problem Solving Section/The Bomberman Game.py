659827aad7e89625f0f93c88d0ccb60ca43e8d9b
tshihab07 <tushar.shihab13@gmail.com>
2025-12-11 23:59:47 +0600

    solve: bomberMan function

--- Problem Solving Section/The Bomberman Game.py
+++ Problem Solving Section/The Bomberman Game.py
@@ -8,14 +8,25 @@ import random
 import re
 import sys
 
-#
-# Complete the 'bomberMan' function below.
-#
-# The function is expected to return a STRING_ARRAY.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. STRING_ARRAY grid
-#
+""" Description: Bomberman lives in a rectangular grid. Each cell in the grid either contains a bomb or nothing at all.
+
+Each bomb can be planted in any cell of the grid but once planted, it will detonate after exactly 3 seconds
+ Once a bomb detonates, it's destroyed — along with anything in its four neighboring cells.
+ This means that if a bomb detonates in cell i, j, any valid cells (i +/- 1, j) and (1, j +/- 1) are cleared.
+ If there is a bomb in a neighboring cell, the neighboring bomb is destroyed without detonating, so there's no chain reaction.
+
+Bomberman is immune to bombs, so he can move freely throughout the grid. Here's what he does:
+
+- Initially, Bomberman arbitrarily plants bombs in some of the cells, the initial state.
+- After one second, Bomberman does nothing.
+- After one more second, Bomberman plants bombs in all cells without bombs, thus filling the whole grid with bombs. No bombs detonate at this point.
+- After one more second, any bombs planted exactly three seconds ago will detonate. Here, Bomberman stands back and observes.
+- Bomberman then repeats steps 3 and 4 indefinitely.
+Note that during every second Bomberman plants bombs, the bombs are planted simultaneously (i.e., at the exact same moment), and any bombs planted at the same time will detonate at the same time.
+
+Given the initial configuration of the grid with the locations of Bomberman's first batch of planted bombs, determine the state of the grid after  seconds.
+
+For example, if the initial grid looks like:"""
 
 def bomberMan(n, grid):
     r = len(grid)
