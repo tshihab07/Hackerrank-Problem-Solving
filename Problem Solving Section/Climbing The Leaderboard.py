1a56111d68e647e585af72a2be1ec0f1b2512f34
tshihab07 <tushar.shihab13@gmail.com>
2025-12-23 23:08:27 +0600

    solve: climbingLeaderboard function

--- Problem Solving Section/Climbing The Leaderboard.py
+++ Problem Solving Section/Climbing The Leaderboard.py
@@ -1,8 +1,7 @@
 import os
 
 
-""" Problem Description:
-An arcade game player wants to climb to the top of the leaderboard and track their ranking.
+""" Problem Description: An arcade game player wants to climb to the top of the leaderboard and track their ranking.
 The game uses Dense Ranking, so its leaderboard works like this:
 
 The player with the highest score is ranked number 1 on the leaderboard. 
