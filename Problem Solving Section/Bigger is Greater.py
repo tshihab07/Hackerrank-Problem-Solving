377bed4dc38d2a82f664e040d9461227c4e6e69c
tshihab07 <tushar.shihab13@gmail.com>
2025-12-23 23:05:22 +0600

    solve: biggerIsGreater function

--- Problem Solving Section/Bigger is Greater.py
+++ Problem Solving Section/Bigger is Greater.py
@@ -1,17 +1,15 @@
+import math
+import os
+import random
+import re
+import sys
+
 """ Problem Description:
 Lexicographical order is often known as alphabetical order when dealing with strings. A string is greater than another string if it comes later in a lexicographically sorted list.
 Given a word, create a new word by swapping some or all of its characters. This new word must meet two criteria:
 - It must be greater than the original word
 - It must be the smallest word that meets the first condition.
 """
-
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
 
 
 def biggerIsGreater(w):
