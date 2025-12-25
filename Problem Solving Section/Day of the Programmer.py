06be3f01d46713e4f5d4e2529ceb46a05a99eaf0
tshihab07 <tushar.shihab13@gmail.com>
2025-12-25 23:54:12 +0600

    solve: dayOfProgrammer function

--- Problem Solving Section/Day of the Programmer.py
+++ Problem Solving Section/Day of the Programmer.py
@@ -19,12 +19,12 @@ Then print it in the format
 """
 
 def dayOfProgrammer(year):
-    # Write your code here
     if year == 1918:
         return "26.09.1918"
-    elif (year < 1918 and year % 4 == 0) or \
-         (year > 1918 and (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0))):
+    
+    elif (year < 1918 and year % 4 == 0) or (year > 1918 and (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0))):
         return f"12.09.{year}"
+    
     else:
         return f"13.09.{year}"
 
