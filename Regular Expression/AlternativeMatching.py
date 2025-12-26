f9bd57299412e6ed169325a5df0422bf47baf6a1
tshihab07 <tushar.shihab13@gmail.com>
2025-12-26 23:31:59 +0600

    Add regex solution: Alternative Matching

--- Regular Expression/AlternativeMatching.py
+++ Regular Expression/AlternativeMatching.py
@@ -7,7 +7,7 @@ Given a test string, S, write a RegEx that matches S under the following conditi
 - The rest of the string must contain only one or more English alphabetic letters (upper and lowercase).
 """
 
-Regex_Pattern = r'^(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[a-zA-Z]+$'	# Do not delete 'r'.
+Regex_Pattern = r'^(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[a-zA-Z]+$'
 
 import re
 
