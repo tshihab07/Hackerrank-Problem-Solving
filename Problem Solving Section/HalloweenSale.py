07122b8048bb2e3f53c7918dd6259e9597fae4c8
tshihab07 <tushar.shihab13@gmail.com>
2025-12-26 23:26:50 +0600

    solve: howManyGames function

--- Problem Solving Section/HalloweenSale.py
+++ Problem Solving Section/HalloweenSale.py
@@ -1,21 +1,8 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
-
-#
-# Complete the 'howManyGames' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER p
-#  2. INTEGER d
-#  3. INTEGER m
-#  4. INTEGER s
-#
 
 def howManyGames(p, d, m, s):
     # Return the number of games you can buy
