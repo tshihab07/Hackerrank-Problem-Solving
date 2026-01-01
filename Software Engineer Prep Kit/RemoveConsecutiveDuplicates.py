182a92a56cae2ac03a921915113f589da84f19cd
tshihab07 <tushar.shihab13@gmail.com>
2026-01-01 23:38:06 +0600

    solve: deleteDuplicates function

---
+++ Software Engineer Prep Kit/RemoveConsecutiveDuplicates.py
@@ -0,0 +1,67 @@
+""" Problem Description: Remove Consecutive Duplicates 
+Write a function "deleteDuplicates" that removes consecutive duplicate nodes in-place,
+retaining only the first node of each code. Return the head of the resulting list.
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(node.data, end='')
+
+        node = node.next
+
+        if node:
+            print(sep, end='')
+
+
+def deleteDuplicates(head):
+    current = head
+
+    while current and current.next:
+        if current.data == current.next.data:
+            current.next = current.next.next
+        else:
+            current = current.next
+
+    return head
+
+if __name__ == '__main__':
+    head_count = int(input().strip())
+
+    head = SinglyLinkedList()
+
+    for _ in range(head_count):
+        head_item = int(input().strip())
+        head.insert_node(head_item)
+
+    result = deleteDuplicates(head.head)
+
+    print_singly_linked_list(result, '\n')
+    print()
